27ef06bdb0500dc3a7dbf012449e01430061f098
lilimarr <liliannwaedozie9@gmail.com>
2025-09-22 20:49:39 -0600

    easydesign1

---
+++ app.js
@@ -0,0 +1,8 @@
+let menuBtn = document.getElementById('menuBtn')
+
+let menuStuff = document.getElementById('menuStuff')
+
+menuBtn.addEventListener('click', () => {
+    menuStuff.classList.toggle("hidden")
+    // alert('hello')
+})
